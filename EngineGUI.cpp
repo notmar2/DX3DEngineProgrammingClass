ac5df3da87fbf7d056c4867da12034f97f71c81a
notmarrow <javierrdzmtz2002@gmail.com>
2023-05-16 12:14:24 -0600

    gui 2

--- EngineGUI.cpp
+++ EngineGUI.cpp
@@ -1,6 +1,6 @@
 #include "EngineGUI.h"
 
-void EngineGUI::init(void* window, ID3D11Device* device, ID3D11DeviceContext* deviceContext) {
+void EngineGUI::init(HWND* window, ID3D11Device* device, ID3D11DeviceContext* deviceContext) {
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
 	ImGuiIO& io = ImGui::GetIO();
@@ -16,23 +16,47 @@ void EngineGUI::init(void* window, ID3D11Device* device, ID3D11DeviceContext* de
 }
 
 void EngineGUI::update() {
+	ImGuiIO& io = ImGui::GetIO();
 	ImGui_ImplDX11_NewFrame();
 	ImGui_ImplWin32_NewFrame();
 	ImGui::NewFrame();
 	
-	static float f = 0.0f;
-	static int counter = 0;
+	if (show_demo_window)
+		ImGui::ShowDemoWindow(&show_demo_window);
 
-	ImGui::Begin("Hello, world!");                          
-	ImGui::Text("This is some useful text.");               
-	ImGui::SliderFloat("float", &f, 0.0f, 1.0f);          
+	// 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+	{
+		static float f = 0.0f;
+		static int counter = 0;
 
-	if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-		counter++;
-	ImGui::SameLine();
-	ImGui::Text("counter = %d", counter);
-	ImGui::End();
-	ImGui::EndFrame();
+		ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+		ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+		ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+		ImGui::Checkbox("Another Window", &show_another_window);
+
+		ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+		ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+		if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+			counter++;
+		ImGui::SameLine();
+		ImGui::Text("counter = %d", counter);
+
+		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+		ImGui::End();
+	}
+
+	// 3. Show another simple window.
+	if (show_another_window)
+	{
+		ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+		ImGui::Text("Hello from another window!");
+		if (ImGui::Button("Close Me"))
+			show_another_window = false;
+		ImGui::End();
+	}
+
 }
 
 void EngineGUI::render() {
--- EngineGUI.h
+++ EngineGUI.h
@@ -6,14 +6,22 @@
 #include "imgui_impl_win32.h"
 #include "imgui_impl_dx11.h"
 #include <imgui_internal.h>
+#include <Windows.h>
 
 class EngineGUI
 {
-public:
-	EngineGUI() {}
+	bool show_demo_window;
+	bool show_another_window;
+	ImVec4 clear_color;
+ public:
+	 EngineGUI() { 
+		show_demo_window = true;
+		show_another_window = false;
+		clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+	 }
 	~EngineGUI() {}
 
-	void init(void* window, ID3D11Device* device, ID3D11DeviceContext* deviceContext);
+	void init(HWND* window, ID3D11Device* device, ID3D11DeviceContext* deviceContext);
 	void update();
 	void render();
 	void shutdown();
